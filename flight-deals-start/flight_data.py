abde4ee38a6e4c3e27de150aa4f17b09064654bb
Szymon Bryniak <105818379+SzymonBryniak@users.noreply.github.com>
2024-10-16 20:41:55 +0100

    day 39 progress

--- flight-deals-start/flight_data.py
+++ flight-deals-start/flight_data.py
@@ -7,6 +7,8 @@ class FlightData:
         self.oAuth_token = {}
         self.locations_end = "https://test.api.amadeus.com/v1/reference-data/locations"
         self.location_by_id = "https://test.api.amadeus.com/v1/reference-data/locations/idtoenter"
+        self.enter_cities()
+        self.location_ids = []
         # self.enter_cities()
 
 
@@ -23,40 +25,67 @@ class FlightData:
 
         response = requests.post(url=endpoint, headers=header, data=data)
         response.raise_for_status()
-        print(response.text)
         self.oAuth_token.update(response.json())
-        print(self.oAuth_token)
 
         return self.oAuth_token['access_token']
 
-
     def enter_cities(self):
         print("Enter 5 cities")
-        while len(self.cities) < 5:
-            self.cities.append(input("Please enter a city name: "))
+        while len(self.cities) < 1:
+            self.cities.append(input("Please enter a city name: ").upper())
 
 
 
-    def get_cities(self):
+    def get_cities(self, counter=0):
+        print(self.cities)
         # curl
         # 'https://test.api.amadeus.com/v1/shopping/flight-destinations?origin=PAR&maxPrice=200' \
         # - H
         # 'Authorization: Bearer ABCDEFGH12345'
         endpoint = self.locations_end
         token = self.get_oAuth_token()
-        print(self.oAuth_token)
+
         header = {
             'Authorization': f'Bearer {token}'
         }
         params = {
             "subType": "CITY",
-            "keyword": "MUNICH"
+            "keyword": f"{self.cities[counter]}"
 
         }
         response = requests.get(url=endpoint, headers=header, params=params)
         response.raise_for_status()
-        print(response.json()["data"][0]['id'])
-    pass
+        try:
+            print(response.json()["data"][0]['id'])
+            self.location_ids.append(response.json()["data"][0]['id'])
+        except IndexError:
+            print(f'No destinations found to {self.cities[counter]} ')
+            print(response.json())
+
+
+
+    def get_airports(self):
+        endpoint = "https://test.api.amadeus.com/v1/reference-data/locations/CMUC"
+        token = self.get_oAuth_token()
+
+        header = {
+            'Authorization': f'Bearer {token}'
+        }
+        # params = {
+        #     "locationId": f"{self.location_ids[0]}"
+        #
+        # }
+        response = requests.get(url=endpoint, headers=header)
+        response.raise_for_status()
+        print(response.json())
+        # try:
+        #     print(response.json()["data"][0]['id'])
+        # except IndexError:
+        #     print(f'No destinations found to {self.cities[0]} ')
+        #     print(response.json())
 
 cities = FlightData()
-cities.get_cities()
+for i in range(1):
+    cities.get_cities(i)
+
+cities.get_airports()
